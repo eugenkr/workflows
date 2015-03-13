18247d8a06ee41df829141086220df5d058d4f4d
Yevgen Kruglyk <ykruglyk@softserveinc.com>
2015-03-13 11:57:20 +0200

    Added coffee script

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,13 @@
  */
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var coffee = require('gulp-coffee');
 
-gulp.task('log', function () {
-    gutil.log('The first Gulp task');
+var coffeeSource = ['components/coffee/*.coffee'];
+
+gulp.task('coffee', function () {
+    gulp.src(coffeeSource)
+        .pipe(coffee({ bare: true }))
+            .on('error', gutil.log)
+        .pipe(gulp.dest('components/scripts'));
 });
