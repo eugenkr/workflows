0e19af103cbfda1e52b63437b2611d6a79685f5e
Yevgen Kruglyk <ykruglyk@softserveinc.com>
2015-03-13 13:44:13 +0200

    Live reloading

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
 var browserify = require('gulp-browserify');
 var compass = require('gulp-compass');
+var connect = require('gulp-connect');
 
 var coffeeSource = ['components/coffee/*.coffee'];
 var jsSource = ['components/scripts/*.js'];
@@ -24,7 +25,8 @@ gulp.task('js', function () {
     gulp.src(jsSource)
         .pipe(concat('script.js'))
         .pipe(browserify())
-        .pipe(gulp.dest('builds/development/js'));
+        .pipe(gulp.dest('builds/development/js'))
+        .pipe(connect.reload());
 });
 
 gulp.task('compass', function () {
@@ -35,7 +37,8 @@ gulp.task('compass', function () {
             style: 'expanded'
         }))
             .on('error', gutil.log)
-        .pipe(gulp.dest('builds/development/css'));
+        .pipe(gulp.dest('builds/development/css'))
+        .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
@@ -44,5 +47,12 @@ gulp.task('watch', function () {
     gulp.watch('components/sass/*.scss', ['compass']);
 });
 
+gulp.task('connect', function () {
+    connect.server({
+        root: 'builds/development/',
+        livereload: true
+    });
+});
+
 // will be run by gulp command without params
-gulp.task('default', ['coffee', 'js', 'compass']);
+gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
