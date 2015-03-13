4ea123b219173f2fe1c3464307892bca067f64b7
Yevgen Kruglyk <ykruglyk@softserveinc.com>
2015-03-13 11:46:26 +0200

    Installed gulp and gulp-util

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * @author Yevgen Kruglyk
+ */
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+
+gulp.task('log', function () {
+    gutil.log('The first Gulp task');
+});
