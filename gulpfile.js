ab4b55817933c7637ce8c6ad1780b478e9730fcf
Yevgen Kruglyk <ykruglyk@softserveinc.com>
2015-03-13 12:05:20 +0200

    Concatenated js

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,20 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
+var concat = require('gulp-concat');
 
 var coffeeSource = ['components/coffee/*.coffee'];
+var jsSource = ['components/scripts/*.js'];
 
 gulp.task('coffee', function () {
     gulp.src(coffeeSource)
         .pipe(coffee({ bare: true }))
             .on('error', gutil.log)
         .pipe(gulp.dest('components/scripts'));
+});
+
+gulp.task('js', function () {
+    gulp.src(jsSource)
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('builds/development/js'));
 });
